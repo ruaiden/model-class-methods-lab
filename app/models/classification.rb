8dc41461bebdf4563362875294da2ccad812f925
Aiden <aiden.marques94@gmail.com>
2021-04-14 19:19:42 -0400

    commit tree ed64370c74b2927cdec82e7cb50cd9cf97a2e229

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -4,10 +4,11 @@ class Classification < ActiveRecord::Base
 
   def self.my_all
     # all
+    Classification.all
   end
 
   def self.longest
-    # Boat.longest.classifications
+    Boat.longest.classifications
   end
 
 end
